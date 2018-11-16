2fa15a12579c20ab8cdd13a70334154774220fba
yevheniia <tymoschuk.jane@gmail.com>
2018-11-16 12:01:31 +0100

    fix for stats pages for new tournaments

--- app/controllers/Battlefy.scala
+++ app/controllers/Battlefy.scala
@@ -17,7 +17,7 @@ class Battlefy @Inject()(ws: WSClient) extends Controller {
   def currentOpponent(name: String): Option[String] = currentOpponents.filter(o => o._1.contains(name) || o._2.contains(name)).flatMap(o => if (o._1.contains(name)) o._2 else o._1).headOption
 
   def currentOpponents: List[(Option[String], Option[String])] = {
-    val info = stageInfo(currentStage).value.sortBy(_.asInstanceOf[JsObject].value("roundNumber").as[Int]).reverse
+    val info = currentStage.map(stageInfo(_).value.sortBy(_.asInstanceOf[JsObject].value("roundNumber").as[Int]).reverse).getOrElse(Seq())
     info.map(o => o.as[JsObject].value("top").as[JsObject] -> o.as[JsObject].value("bottom").as[JsObject])
       .map(o => (if (o._1.keys.contains("team")) Some(o._1) else None) -> (if (o._2.keys.contains("team")) Some(o._2) else None))
       .map(o => o._1.map(_.value("team").as[JsObject].value("name").as[String]) -> o._2.map(_.value("team").as[JsObject].value("name").as[String])).toList
@@ -58,8 +58,8 @@ class Battlefy @Inject()(ws: WSClient) extends Controller {
     }), Duration.apply(30, TimeUnit.SECONDS))
   }
 
-  def currentStage: String = getTournamentInfo(getCurrentTournament.battlefy_id).value("stages").asInstanceOf[JsArray]
-    .value.toList.sortBy(o => o.asInstanceOf[JsObject].value("startTime").as[String]).reverse.head.asInstanceOf[JsObject].value("_id").as[String]
+  def currentStage: Option[String] = getTournamentInfo(getCurrentTournament.battlefy_id).value("stages").asInstanceOf[JsArray]
+    .value.toList.sortBy(o => o.asInstanceOf[JsObject].value("startTime").as[String]).reverse.headOption.map(_.asInstanceOf[JsObject].value("_id").as[String])
 
   def stageInfo(stage: String): JsArray = Await.result(ws.url(stage_info(stage)).get().map(response => {
     Json.parse(response.body).asInstanceOf[JsArray]
