feb69e28a6ed79e9fc9006e0e412d8b09d46ce67
yevheniia <tymoschuk.jane@gmail.com>
2018-12-08 18:25:21 +0100

    casters list is left aligned

--- app/controllers/Graphics.scala
+++ app/controllers/Graphics.scala
@@ -230,7 +230,7 @@ class Graphics @Inject()(fs: FileSystem, eternalWarcry: EternalWarcry, database:
     g.drawString(castersStr, center(g, castersStr), 35)
     val casters = list.split("\n")
     for (i <- casters.indices) {
-      g.drawString(casters(i), center(g, casters(i)), 50 + 40 * (i + 1))
+      g.drawString(casters(i), 0, 50 + 40 * (i + 1))
     }
     g.dispose()
 
